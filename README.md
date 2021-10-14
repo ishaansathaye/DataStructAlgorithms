180e008cd86c7d0be8742cfec47f592420a730a7
ishaansathaye <ishaan.sathaye@gmail.com>
2021-10-13 23:16:35 -0700

    notation converting

--- README.md
+++ README.md
@@ -17,10 +17,35 @@
 - Notation Rules:
     1. Keep fastest growing term (time = a*n)
     2. Drop constants (time = 0(n))
-- n refers to iterations
+- Reasoning behind Rules
+    - BigO refers to very large value of n
+    - If you have a function: time = 5*n^2 + 3*n + 20
+        - **When the value of n is very large -> the second and third terms become irrelevant**
+- **n refers to iterations**
     - number of computations
-- increasing size input -> time is almost constant
+- Increasing size input -> time is almost constant
     - time = a
     - Keep fastest growing term
     - Drop constants (a) -> time = **0(1)**
         - Order of 1
+- **Everything above also applies to Space Complexity**
+- Example of Binary Search
+    - Do not want to find number in a list using O(n)
+    - If there are billion numbers then program has to do billion iterations 
+<p align="center"><img src="Images/binarysearchexample.png" width="450"></p>
+
+- Binary Search (Search for 68 in a sorted list)
+    - Find middle number in list and compare to 68 (less than so discard all items to the left)
+    - Find middle number and see if less than and then discard left
+    - Again divide array by 2 and take middle element is 68
+        - Iteration 1 = n/2
+        - Iteration 2 = (n/2)/2 = n/2^2
+        - Iteration 3 = (n/2^2)/2 = n/2^3
+    - Process to 3 iterations, while O(n) would take 7 since 68 in seventh position
+    - Iteration k = n/2^k
+- **Convert Iteration k = n/2^k to BigO**
+    - 1 = n/2^k
+        - 1 is worse case scenario
+    - n = 2^k
+    - log122
+
--- timecomplexity.py
+++ timecomplexity.py
@@ -1,33 +1,59 @@
-#Order of n: O(n)
-#Depends on number of iterations
+print()
+print("Order of n: O(n):")
 print('---------------------------')
 def getsqrnum(num):
-    '''this has time complexity of order n'''
     sqrnum = []
     for n in num:
         sqrnum.append(n*n)
     return sqrnum
 numbers = [2,5,8,9]
 print(getsqrnum(numbers))
-print()
+print("Depends on number of iterations")
+print("This has time complexity of order n")
+print('---------------------------')
 
-#Order of 1: O(1)
-#Increasing input still means constant time
+print()
+print("Order of 1: O(1):")
+print('---------------------------')
 def find_first_pe(prices, eps, index):
     pe = prices[index]/eps[index]
     return pe
-#doing a constant operation
-#does not matter about the size of eps
-#time execution remains constrant
+print("Increasing input still means constant time")
+print("Doing a constant operation")
+print("Does not matter about the size of eps")
+print("Time execution remains constrant")
+print('---------------------------')
 
-#order of n^2: 0(n^2)
+print()
+print("Order of n^2: 0(n^2):")
+print('---------------------------')
 newset = [3,6,2,4,3,6,8,9]
 for i in range(len(newset)):
     for j in range(i+1, len(newset)):
         if newset[i] == newset[j]:
-            print(newset[i] + " is a duplicate")
+            print(str(newset[i]) + " is a duplicate")
             break
-#time = a*n^2 + b
-#Keep fastest frowing term and drop constants ->
-#
+print("time = a*n^2 + b")
+print("Keep fastest frowing term and drop constants ->")
+print("Becomes 0(n^2)")
+print('---------------------------')
 
+print()
+print("Order of n^2: 0(n^2):")
+print('---------------------------')
+print("Below has n^2 iterations")
+newNumbers = [3,6,2,4,3,6,8,9]
+duplicate = None
+for i in range(len(newNumbers)):
+    for j in range(i+1, len(newNumbers)):
+        if newNumbers[i] == newNumbers[j]:
+            duplicate = newNumbers[i]
+            break
+print("Below has n iterations")
+for i in range(len(newNumbers)):
+    if newNumbers[i] == duplicate:
+        print(i)
+print("time = a*n^2 + b*n + c")
+print("Use rules and keep fastest growing and drop constants ->")
+print("Becomes 0(n^2)")
+print('---------------------------')
