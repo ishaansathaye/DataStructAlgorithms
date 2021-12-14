c9327ece46a7e444bc953866568bc96517d38307
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2021-12-13 17:54:02 -0800

    update to BST

--- BinaryTree/BSTProblems/moreBST.py
+++ BinaryTree/BSTProblems/moreBST.py
@@ -3,6 +3,7 @@
 # 3. calculate_sum(): calculates sum of all elements
 # 4. post_order_traversal(): performs post order traversal of a binary tree
 # 5. pre_order_traversal(): performs pre order traversal of a binary tree
+
 class BinarySearchTreeNode:
     def __init__(self, data):
         self.data = data
@@ -10,62 +11,59 @@ class BinarySearchTreeNode:
         self.right = None
     
     def add_child(self, data):
-        if data == self.data: #if the value already exists
+        if data == self.data:
             return
         
-        if data < self.data: #if value less than current node then add data in left subtree
-            if self.left: #if current node has a left node then it is not a leaf node
-                self.left.add_child(data) #use recursion on the left subtree of current node
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
             else:
                 self.left = BinarySearchTreeNode(data)
-        else: #if value greater than current node then add data in right subtree
+        else:
             if self.right:
                 self.right.add_child(data)
             else:
                 self.right = BinarySearchTreeNode(data)
     
-    def in_order_traversal(self): #traverse left tree, then base node, then right subtree
+    def in_order_traversal(self):
         elements = []
 
-        #traverse left tree
         if self.left:
-            elements += self.left.in_order_traversal() #use recursion to keep going to leftmost node of left subtree and adding to list
+            elements += self.left.in_order_traversal()
 
-        #traverse base node
         elements.append(self.data)
 
-        #traverse right subtree
         if self.right:
             elements += self.right.in_order_traversal()
 
-        return elements #returns elements in an ascending order
+        return elements
 
-    def search(self, value): #searching for a value
+    def search(self, value):
         if self.data == value:
             return True
         
-        if value < self.data: #value might be in left subtree
+        if value < self.data:
             if self.left:
-                return self.left.search(value) #use recursion to further go into left subtree
-            else: #if there is no left children of the base node
+                return self.left.search(value)
+            else:
                 return False
 
-        if value > self.data: #might be in right subtree
+        if value > self.data:
             if self.right:
                 return self.right.search(value)
             else:
                 return False
 
 def build_tree(elements):
-    root = BinarySearchTreeNode(elements[0]) #creates a root node 
+    root = BinarySearchTreeNode(elements[0])
 
     for i in range(1, len(elements)): 
-        root.add_child(elements[i]) #adding all children and sub-children to root node
+        root.add_child(elements[i])
     
     return root
 
 if __name__ == '__main__':
-    numbers = [17, 4, 1, 20, 9, 23, 18, 34, 18, 4] #only will include unique values
+    numbers = [17, 4, 1, 20, 9, 23, 18, 34, 18, 4]
     numbers_tree = build_tree(numbers)
     print(numbers_tree.in_order_traversal())
     print()
