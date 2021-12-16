45558a1c72b786bbb6ea09eb6adaa6b6ab557b23
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2021-12-15 21:55:14 -0800

    update  cd /Users/ishaansathaye/GitHub/DataStructAlgorithms ; /usr/bin/env /Users/ishaansathaye/.pyenv/versions/3.10.0/bin/python /Users/ishaansathaye/.vscode/extensions/ms-python.python-2021.12.1559732655/pythonFiles/lib/python/debugpy/launcher 50910 -- /Users/ishaansathaye/GitHub/DataStructAlgorithms/Graph/graph.py

--- Graph/graph.py
+++ Graph/graph.py
@@ -24,20 +24,30 @@ class Graph:
         for node in self.graph_dict[start]: #iterates through the values of the dictionary
             if node not in path: #check if the destination has been visited or not
                 new_paths = self.get_paths(node, end, path) #NOTE: use recursion to get the end destination by going through all the possible values of start key
-                #NOTE: use debugging to follow recursion
+                #NOTE: use debugging to follow recursion -> **difficult to follow**
                 for p in new_paths: #iterates through different paths
                     paths.append(p) #adds different paths to a list
         
         return paths #returns a list with all possible paths
 
-    def get_shortest_path(self, start, end, path=[]): #finds shortest paths based on number of stops or edges
+    def get_shortest_path(self, start, end, path=[]): #finds shortest paths based on number of stops
         path = path + [start]
 
         if start == end:
             return path
 
         if start not in self.graph_dict:
-            return []
+            return None
+
+        shortest_path = None
+        for node in self.graph_dict[start]:
+            if node not in path:
+                sp = self.get_shortest_path(node, end, path)
+                if sp: #check because function can return None
+                    if shortest_path is None or len(sp) < len(shortest_path): #check if the shortest_path is empty or if the current short path length is less than the current shortest path length
+                        shortest_path = sp
+        return shortest_path
+
 
 if __name__ == '__main__':
     routes = [ #use tuples to simulate routes between cities
@@ -86,6 +96,14 @@ if __name__ == '__main__':
 
     print()
 
-    
+    start = "Mumbai"
+    end = "New York"
+    print(f"Shortest Path between {start} and {end}:", route_graph.get_shortest_path(start, end))
+
+    print()
+
+    start = "Paris"
+    end = "New York"
+    print(f"Shortest Path between {start} and {end}:", route_graph.get_shortest_path(start, end))
 
     print()
