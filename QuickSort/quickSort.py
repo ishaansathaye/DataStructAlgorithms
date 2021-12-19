3f7231d44a5418b051fd7a71a0d8263697636703
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2021-12-18 19:10:12 -0800

    update quick sort

--- QuickSort/quickSort.py
+++ QuickSort/quickSort.py
@@ -2,13 +2,10 @@ def swap(a, b, arr):
     if a != b:
         arr[a], arr[b] = arr[b], arr[a]
 
-
-def partition(elements): #creates a partition by putting an element in the correct position and create left and right sides
-    pivot_index = 0
+#need start and end for recursion
+def partition(elements, start_pointer, end_pointer): #creates a partition by putting an element in the correct position and create left and right sides
+    pivot_index = start_pointer
     pivot = elements[pivot_index]
-
-    start_pointer = pivot_index+1
-    end_pointer = len(elements) - 1
 
     while start_pointer < end_pointer: #keeps process going until the pointers cross
         while elements[start_pointer] <= pivot: #do until found element greater than pivot
@@ -22,14 +19,15 @@ def partition(elements): #creates a partition by putting an element in the corre
     
     swap(pivot_index, end_pointer, elements) #swaps end pointer and pivot if the pointers cross
 
-    return elements
+    return end_pointer #position of the pivot at the end of partitioning
 
-
-
-
-def quick_sort(elements): #Hoare Partitioning Method used
-    return partition(elements)
+#need start and end for recursion
+def quick_sort(elements, start_pointer, end_pointer): #Hoare Partitioning Method used
+    if start_pointer < end_pointer: #keep repeating process until start and end are equal which means that it is sorted
+        pi = partition(elements, start_pointer, end_pointer) #partition index
+        quick_sort(elements, start_pointer, pi-1) #run quick sort on left partition
+        quick_sort(elements, pi+1, end_pointer) #run quick sort on right partition
 
 if __name__ == '__main__':
     elements = [11, 9, 29, 7, 3, 15, 28]
-    print(quick_sort(elements))
+    print(quick_sort(elements, 0, len(elements)-1))
