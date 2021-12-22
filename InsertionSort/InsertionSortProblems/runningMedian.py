f9c2521740819eb0d1d39bb1668279d18718c46d
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2021-12-21 21:39:24 -0800

    codebasics solution uses input

--- InsertionSort/InsertionSortProblems/runningMedian.py
+++ InsertionSort/InsertionSortProblems/runningMedian.py
@@ -33,9 +33,35 @@ def insertion_sort(elements):
         elements[j+1] = anchor
     print_median(elements, len(elements))
 
+#codebasics solution
+def place_to_insert(array, key):
+    index = 0
+    for i in array:
+        if i > key:
+            break
+        else:
+            index += 1
+    return index
+def insert_to_sorted(array, key):
+    index = place_to_insert(array, key)
+    return array[0:index]+[key]+array[index:]
+
 if __name__ == '__main__':
     elements  = [2, 1, 5, 7, 2, 0, 5]
     insertion_sort(elements)
-    # print(elements)
+    
     print()
     
+    #codebasics solution - uses input of elements in list
+    stream = []
+    count = 0
+    while(True):
+        i = int(input())
+        count += 1
+        stream = insertion_sort(stream, i)
+        if count % 2 == 1:
+            print(f"Median of {stream} : {stream[(count)//2]}")
+        else:
+            i1 = count//2
+            i2 = (count//2) - 1
+            print(f"Median of {stream} : {(stream[i1] + stream[i2])/2}")
