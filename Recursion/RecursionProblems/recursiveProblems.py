79c79192059ebcb4b4fb51364b89654c6311aefb
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2021-12-25 18:45:46 -0800

    recursive prob 11

--- Recursion/RecursionProblems/recursiveProblems.py
+++ Recursion/RecursionProblems/recursiveProblems.py
@@ -1,6 +1,7 @@
 #https://www.w3resource.com/python-exercises/data-structures-and-algorithms/python-recursion.php
 
 print()
+
 # 1. Write a Python program to calculate the sum of a list of numbers.  
 def solution(listofnum, index):
     if index == len(listofnum)-1:
@@ -27,6 +28,7 @@ def alt_solution2(n, base):
     else:
         return alt_solution2(n // base, base) + conver_tString[n % base]
 
+print("-")
 print(alt_solution2(2835, 16))
 
 print()
@@ -43,6 +45,7 @@ def alt_solution3(listThings):
             sumList += element
     return sumList
 
+print("-")
 print(alt_solution3([1, 2, [3,4], [5,6]]))
 
 print()
@@ -82,26 +85,76 @@ print()
 def alt_solution6(n):
     if n == 0:
         return 0
-    return n % 10 + alt_solution6(n // 10) #adding the last number (gotten by taking remainder of dividing by 10) and adding the 
+    return n % 10 + alt_solution6(n // 10) #adding the last number (gotten by taking remainder of dividing by 10) and adding of recursion (parameter of number divided by 10 and floored)
 
+print("-")
 print(alt_solution6(345))
 
-# print(solution6(345))
+print()
 
 # 7. Write a Python program to calculate the sum of the positive integers of n+(n-2)+(n-4)... (until n-x =< 0).
 # Test Data: 
 # sum_series(6) -> 12
 # sum_series(10) -> 30 
+def solution7(n):
+    if n == 0:
+        return 0
+    return n+solution7(n-2)
+
+print(solution7(10))
+print("-")
+
+print()
 
 # 8. Write a Python program to calculate the harmonic sum of n-1.
 # Note: The harmonic sum is the sum of reciprocals of the positive integers. 
+def solution8(n):
+    if n == 1:
+        return 1
+    return (1/n) + solution8(n-1)
+
+print(solution8(4))
+print("-")
+
+print()
 
 # 9. Write a Python program to calculate the geometric sum of n-1.
 # Note: In mathematics, a geometric series is a series with a constant ratio between successive terms. 
+def alt_solution9(n):
+    if n < 0: #raised to the power of 0 is 1 so anything less need to return 0
+        return 0
+    return (1 / pow(2, n)) + alt_solution9(n-1) #raising to the power of n because geometric series with r = number raised to n
+
+print("-")
+print(alt_solution9(4))
+
+print()
 
 # 10. Write a Python program to calculate the value of 'a' to the power 'b'.  
 # Test Data : 
 # (power(3,4) -> 81 
+def solution10(a, b):
+    if b == 1:
+        return a
+    return a*solution10(a, b-1)
 
+print(solution10(3, 4))
+print("-")
 
-# 11. Write a Python program to find the greatest common divisor (gcd) of two integers.  
+print()
+
+# 11. Write a Python program to find the greatest common divisor (gcd) of two integers. 
+def alt_solution11(a, b):
+	low = min(a, b)
+	high = max(a, b)
+
+	if low == 0:
+		return high
+	elif low == 1:
+		return 1
+	else:
+		return alt_solution11(low, high%low)
+
+print(Recurgcd(12,14))
+
+print()
